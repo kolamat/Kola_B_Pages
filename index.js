8c24a74c701f02b7526b2205fc41b8c895f1b79e
kolamat <matanmidupe004@gmail.com>
2025-02-11 23:18:43 +0100

    add personal data and enhance message function in index.js

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const namesArrays = [
+  "john",
+  "samuel",
+  "jesus",
+  "job",
+  "jack",
+  "jackson",
+  "tomiwa",
+  "yusuf",
+  "sam",
+  "joshua",
+  "kate",
+  "david",
+  "dames",
+  "james",
+  "jacob",
+  "emmanuel",
+];
+
 const images = [
   "RedRoses.jpg",
   "goldrose.jpg",
@@ -26,12 +45,13 @@ const personal = [
 
 let body = document.querySelector("body");
 body.style.textAlign = "center";
-body.style.scrollBehavior = "none";
+body.style.scrollBehavior = "smooth";
 body.style.height = "100vh";
 body.style.background = "#c46666";
 body.style.overflowX = "hidden";
 body.style.color = "white";
 body.style.margin = "0px";
+body.style.padding = "0px 20px 0px 5px";
 body.style.fontFamily = "'Crimson Text', serif";
 
 let heading = document.createElement("H1");
@@ -68,7 +88,7 @@ document.getElementById("div").appendChild(img1);
 
 let pTag = document.createElement("p");
 pTag.textContent =
-  "As much as I really wanna date JOHNSON at the same I really don’t want to";
+  "As much as I really wanna date JOHNSON at the same I really don’t want to,";
 pTag.style.fontWeight = "bold";
 document.getElementById("div").appendChild(pTag);
 
@@ -80,7 +100,7 @@ img2.style.height = "310px";
 document.getElementById("div").appendChild(img2);
 
 let h2 = document.createElement("H2");
-h2.textContent = "But am starting to fall for him";
+h2.textContent = "But am starting to fall for him.";
 document.querySelector("body").appendChild(h2);
 
 let buttonContainer = document.createElement("div");
@@ -89,7 +109,7 @@ buttonContainer.style.display = "flex";
 buttonContainer.style.flexWrap = "wrap";
 buttonContainer.style.gap = "20px";
 buttonContainer.style.justifyContent = "center";
-document.querySelector("body").appendChild(buttonContainer);
+body.appendChild(buttonContainer);
 
 let clearButton = document.createElement("img");
 clearButton.src = "./reset.jpg";
@@ -98,7 +118,7 @@ clearButton.style.cursor = "pointer";
 clearButton.style.borderRadius = "10px";
 // clearButton.style.borderColor = "#00fff0";
 clearButton.addEventListener("click", resetButton);
-document.getElementById("buttonContainer").appendChild(clearButton);
+buttonContainer.appendChild(clearButton);
 
 let button = document.createElement("img");
 button.src = images[2];
@@ -123,6 +143,8 @@ paraGraphAnchor.href = "https://wa.me/+2348150672809";
 paraGraphAnchor.id = "paraGraphAnchor";
 paraGraphAnchor.style.fontWeight = "bold";
 paraGraphAnchor.style.fontSize = "15px";
+paraGraphAnchor.style.textDecoration = "none";
+paraGraphAnchor.style.fontWeight = "bold";
 paraGraphAnchor.style.padding = "15px";
 paraGraphAnchor.style.color = "#aae4aa";
 paraGraphAnchor.style.width = "auto";
@@ -154,16 +176,21 @@ function numberParaGraph() {
 }
 
 function Roses() {
-  // This const code makes only the image to display once and not multiple times
-  const emptyContent = document.getElementById("paraGraphAnchor");
-  emptyContent.textContent = "";
+  // This const code makes the image and content to display once and not multiple times
+  const content = document.getElementById("paraGraphAnchor");
+  content.textContent = "Will You Be My VAL??";
+  content.style.display = "flex";
+  content.style.flexDirection = "column";
+  content.style.alignItems = "center";
+  content.style.color = "#493434";
+  content.style.fontSize = "20px";
 
   let redRose = document.createElement("img");
   redRose.src = images[0];
   redRose.style.width = "300px";
   redRose.style.borderRadius = "10px 20px";
   redRose.style.marginTop = "20px";
-  document.getElementById("paraGraphAnchor").appendChild(redRose);
+  paraGraphAnchor.appendChild(redRose);
 }
 
 function Quotes() {
@@ -248,3 +275,9 @@ function tryReturn() {
 }
 
 tryReturn();
+
+// document.getElementById("pNames").appendChild(namesArrays);
+
+// const pNames = document.createElement("p");
+// pNames.id = "pNames";
+// document.querySelector("body").appendChild(pNames);
